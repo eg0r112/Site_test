a7c9387ceec80bc8bfc5284aca38a5be5cc4e197
eg0r112 <Vedromakaron@gmail.com>
2024-12-06 21:37:42 +0300

    готово

---
+++ script.js
@@ -0,0 +1,109 @@
+function Yes1(){
+    var element = document.getElementById('otst11');
+    var element1 = document.getElementById('otst12');
+    var element2 = document.getElementById('otst13');
+    var element3 = document.getElementById('otst1');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes2(){
+    var element = document.getElementById('otst23');
+    var element1 = document.getElementById('otst22');
+    var element2 = document.getElementById('otst21');
+    var element3 = document.getElementById('otst2');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes3(){
+    var element = document.getElementById('otst33');
+    var element1 = document.getElementById('otst32');
+    var element2 = document.getElementById('otst31');
+    var element3 = document.getElementById('otst3');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes4(){
+    var element = document.getElementById('otst41');
+    var element1 = document.getElementById('otst42');
+    var element2 = document.getElementById('otst43');
+    var element3 = document.getElementById('otst4');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes5(){
+    var element = document.getElementById('otst51');
+    var element1 = document.getElementById('otst52');
+    var element2 = document.getElementById('otst53');
+    var element3 = document.getElementById('otst5');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes6(){
+    var element = document.getElementById('otst61');
+    var element1 = document.getElementById('otst62');
+    var element2 = document.getElementById('otst63');
+    var element3 = document.getElementById('otst6');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes7(){
+    var element = document.getElementById('otst71');
+    var element1 = document.getElementById('otst72');
+    var element2 = document.getElementById('otst73');
+    var element3 = document.getElementById('otst7');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes8(){
+    var element = document.getElementById('otst83');
+    var element1 = document.getElementById('otst82');
+    var element2 = document.getElementById('otst81');
+    var element3 = document.getElementById('otst8');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes9(){
+    var element = document.getElementById('otst91');
+    var element1 = document.getElementById('otst92');
+    var element2 = document.getElementById('otst93');
+    var element3 = document.getElementById('otst9');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
+
+function Yes10(){
+    var element = document.getElementById('otst101');
+    var element1 = document.getElementById('otst102');
+    var element2 = document.getElementById('otst103');
+    var element3 = document.getElementById('otst10');
+    element.style.backgroundColor = "rgb(0, 255, 42)";
+    element1.style.backgroundColor = "rgb(255, 0, 0)";
+    element2.style.backgroundColor = "rgb(255, 0, 0)";
+    element3.style.backgroundColor = "rgb(255, 0, 0)";
+}
